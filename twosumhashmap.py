5f0b68211c26dfb3ff47f2dd794119e2686d269e
Jadesola Bejide <vm21106@bristol.ac.uk>
2022-04-20 16:59:23 +0100

    twosum more robust

--- twosumhashmap.py
+++ twosumhashmap.py
@@ -2,11 +2,13 @@ from typing import List
 
 def twoSum(self, nums: List[int], target: int) -> List[int]:
     hashtable = {}
-    currentValue = nums[0]
-    hashtable[nums[0]] = 0
-    for i in range(1, len(nums)):
-        if nums[i] + currentValue == target:
-            return [hashtable[currentValue], i]
+    
+    for i in range(0, len(nums)):
+        diff = target - nums[i]
+        if diff in hashtable and hashtable[diff]!=i:
+            return [i, hashtable[diff]]
         else:
-            hashtable[nums[i]] = i
-            currentValue = nums[i]
+            if nums[i] in hashtable:
+                hashtable[nums[i]] = i+1
+            else:
+                hashtable[nums[i]] = i
