1cadec85bb158b352a987ba800a88b3a50133534
Jadesola Bejide <vm21106@bristol.ac.uk>
2022-05-09 10:36:48 +0100

    Max Subarray - DP Approach

---
+++ kadanesmaxsubarray.py
@@ -0,0 +1,38 @@
+#O(n) solution - 1 subproblem needed to compute current value
+def maxSubarray(arr):
+    n = len(arr)
+    
+    #array to compute optimal solution via the bottom up appraoch
+    m = [-1]*(n-1)
+    
+    m[0] = arr[0]
+    q = arr[0]
+
+    #variables to track the optimal cost
+    start = 0
+    end = n
+
+    for i in range(1, n-1):
+        if m[i-1] < 0:
+            m[i] = arr[i]
+            start = i
+        else:
+            m[i] = arr[i] + m[i-1]
+            
+        q = max(q, m[i])
+        if q == m[i]:
+            end -= 1
+
+    return q, start, end
+
+def printSolution(soln):
+    return pp[0] + " " + str(soln[0]) + "\n" + str(pp[1]) + " " + str(soln[1]) + " " +  pp[2] + " " +  str(soln[2])
+
+
+a = [-25, 20, -3, -16, -23, 18, 20, -7, 12, -5, 1]
+
+pp = ["The optimal cost is:", "Start:", "End:"]
+soln = maxSubarray(a)
+
+print(printSolution(soln))
+    
